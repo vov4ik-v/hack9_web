8f7d288e4b02bf9f84632d61eb0d3369ad464bb1
vov4ik-v <volodymyr.vasylyshyn05@gmail.com>
2025-01-18 18:19:30 +0200

    initial commit

---
+++ src/app/fonts.ts
@@ -0,0 +1,19 @@
+import { Press_Start_2P} from "next/font/google";
+import LocalFont from 'next/font/local'
+
+export const press_start = LocalFont(
+    {
+    preload: true,
+    src: '../fonts/PressStart2P-Regular.ttf'
+    }
+);
+// export const inter = Inter({
+//     preload: true,
+//     weight: "400",
+//     subsets: ['cyrillic', 'latin']
+// });
+// export const inter_bold = Inter({
+//     preload: true,
+//     weight: "700",
+//     subsets: ['cyrillic', 'latin']
+// });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'hack-dark-green' : '#20441d',
+        'hack-green' : '#33F569'
+      },
+      scale: {
+        102: "1.02",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
